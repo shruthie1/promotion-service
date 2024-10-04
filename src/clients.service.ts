3620a557bea32199def521ee1932afea2a05ed93
SSK <sheetysai>
2024-10-05 00:34:15 +0530

    hmm okay

--- src/clients.service.ts
+++ src/clients.service.ts
@@ -4,13 +4,13 @@ export async function getClient() {
     try {
         const result = await fetchWithTimeout(`${process.env.repl}/getme`);
         if (result.data) {
-            return result.data
+            return result?.data
         } else {
             const client = await fetchWithTimeout(`${process.env.uptimeChecker}/forward/clients/${process.env.clientId}`);
-            return client.data
+            return client?.data
         }
     } catch (error) {
         const client = await fetchWithTimeout(`${process.env.promoteChecker}/clients/${process.env.clientId}`);
-        return client.data
+        return client?.data
     }
 }
