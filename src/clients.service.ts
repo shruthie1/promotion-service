57630ee5391a127c2864962696d815afb2c34649
SSK <sheetysai>
2024-10-05 14:09:18 +0530

    fixes

--- src/clients.service.ts
+++ src/clients.service.ts
@@ -1,19 +1,20 @@
 import { fetchWithTimeout } from "./fetchWithTimeout";
 
 export async function getClient() {
+    const url = `${process.env.repl}/getme`;
     try {
-        const result = await fetchWithTimeout(`${process.env.repl}/getme`);
-        console.log(result)
+        const result = await fetchWithTimeout(url);
+        console.log(result.data)
         if (result.data) {
             return result?.data
         } else {
-            const client = await fetchWithTimeout(`${process.env.uptimeChecker}/clients/${process.env.clientId}`);
-            console.log(client)
+            const client = await fetchWithTimeout(`${process.env.tgcms}/forward?url=${url}`);
+            console.log(client.data)
             return client?.data
         }
     } catch (error) {
-        const client = await fetchWithTimeout(`${process.env.promoteChecker}/forward/clients/${process.env.clientId}`);
-        console.log(client)
+        const client = await fetchWithTimeout(`${process.env.promoteChecker}/bridge/forward?url=${url}`);
+        console.log(client.data)
         return client?.data
     }
 }
