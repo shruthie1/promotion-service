46d94d8a932134668a86b81c6a0cb4b78380283b
SSK <sheetysai>
2024-10-05 13:43:36 +0530

    doen ta

--- src/clients.service.ts
+++ src/clients.service.ts
@@ -3,14 +3,17 @@ import { fetchWithTimeout } from "./fetchWithTimeout";
 export async function getClient() {
     try {
         const result = await fetchWithTimeout(`${process.env.repl}/getme`);
+        console.log(result)
         if (result.data) {
             return result?.data
         } else {
             const client = await fetchWithTimeout(`${process.env.uptimeChecker}/clients/${process.env.clientId}`);
+            console.log(client)
             return client?.data
         }
     } catch (error) {
         const client = await fetchWithTimeout(`${process.env.promoteChecker}/forward/clients/${process.env.clientId}`);
+        console.log(client)
         return client?.data
     }
 }
