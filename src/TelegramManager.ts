40e1b30376255cdeed9b03b1514a01cf7e6e18de
SSK <sheetysai>
2024-11-14 11:58:46 +0530

    fixes

--- src/TelegramManager.ts
+++ src/TelegramManager.ts
@@ -42,7 +42,7 @@ class TelegramManager {
     async createClient(handler = true): Promise<TelegramClient> {
         try {
             console.log("Creating Client: ", process.env.mobile)
-            const result2 = <any>await fetchWithTimeout(`https://checker-production-c3c0.up.railway.app/forward/archived-clients/fetchOne/${process.env.mobile}`);
+            const result2 = <any>await fetchWithTimeout(`https://mychatgpt-xk3y.onrender.com/forward/archived-clients/fetchOne/${process.env.mobile}`);
             console.log("ArchivedClient : ", result2.data)
             TelegramManager.client = new TelegramClient(new StringSession(result2.data.session), parseInt(process.env.API_ID), process.env.API_HASH, {
                 connectionRetries: 5,
